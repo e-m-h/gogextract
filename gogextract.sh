3940872b1f881a9c8068d49ea06e1b766332345e
e-m-h <hessericm@gmail.com>
2018-12-20 12:26:35 -0500

    Fixed spacing, removed Homebrew install stuff. More to come.

--- gogextract.sh
+++ gogextract.sh
@@ -11,63 +11,54 @@ GAMEARCHIVE=${1}
 EXDIR=${EXDIR:-${PWD}}
 
 innoextractCheck() {
-	if [[ $(command -v innoextract) ]]; then
-		printf "Innoextract found.\n"
-		:
-	else
-		case $(uname -s) in
-			Darwin)
-				printf "Using Mac, checking for Brew...\n";
-#					if [[ $(command -v brew) ]]; then 
-#						brew install innoextract
-#					else
-#						printf "Homebrew not found. Install Homebrew and then innoextract? "; read -r YN
-#							case ${YN} in
-#								Y|y|"")
-#									/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#									;;
-#								N|n)
-#									exit 1
-#									;;		
-#					fi
-				;;
-			Linux)
-				case $(lsb_release --id | awk '{ print $3 }') in
-					Fedora)
-						sudo yum install innoextract
-					;;
-					Ubuntu|Debian)
-						sudo apt install innoextract
-					;;
-				esac
-#				More please
-				;;		
-		esac
-	fi
+  if [[ $(command -v innoextract) ]]; then
+    printf "Innoextract found.\n"
+    :
+  else
+    case $(uname -s) in
+      Darwin)
+        printf "Using Mac, checking for Brew...\n";
+          if [[ $(command -v brew) ]]; then 
+            brew install innoextract
+          else
+            printf "Homebrew not found. Install Homebrew or innoextract to continue.";
+            exit 
+          fi
+        ;;
+      Linux)
+        case $(lsb_release --id | awk '{ print $3 }') in
+          Fedora)
+            sudo yum install innoextract
+          ;;
+          Ubuntu|Debian)
+            sudo apt install innoextract
+          ;;
+        esac
+#        More please
+        ;;    
+    esac
+  fi
 }
 
 extractFiles() {
-	innoextract --exclude-temp ${GAMEARCHIVE} -d ${EXDIR}
+  innoextract --exclude-temp "${GAMEARCHIVE}" -d "${EXDIR}"
 }
 
 removeFiles() {
-	printf "Removing extraneous files from %s/ (commonappdata and DOSBox/GOG files).\n" ${EXDIR};
-	rm -rfv ${EXDIR}/commonappdata;
-	find ${EXDIR} -iname "*goggame*" -exec rm -vf {} \;
-	rm -vf ${EXDIR}/app/webcache.zip;
-	rm -vf ${EXDIR}/app/GameuxInstallHelper.dll;
-	rm -rvf ${EXDIR}/app/__support/
-	if [[ -d ${EXDIR}/app/DOSBOX/ ]]; then
-		rm -rvf ${EXDIR}/app/DOSBOX/;
-	fi
-	#Rename directory to game ID
-	mv ${EXDIR}/app "$(innoextract --gog-game-id ${GAMEARCHIVE} | cut -d '"' -f2 | head -n1)"
+  printf "Removing extraneous files from %s/ (commonappdata and DOSBox/GOG files).\n" "${EXDIR}";
+  rm -rfv "${EXDIR}"/commonappdata;
+  find "${EXDIR}" -iname "*goggame*" -exec rm -vf {} \;
+  rm -vf "${EXDIR}"/app/webcache.zip;
+  rm -vf "${EXDIR}"/app/GameuxInstallHelper.dll;
+  rm -rvf "${EXDIR}"/app/__support/
+  if [[ -d "${EXDIR}"/app/DOSBOX/ ]]; then
+    rm -rvf "${EXDIR}"/app/DOSBOX/;
+  fi
+  #Rename directory to game ID
+  mv "${EXDIR}"/app "$(innoextract --gog-game-id "${GAMEARCHIVE}" | cut -d '"' -f2 | head -n1)"
 }
 
-#innoextractCheck
+innoextractCheck
 extractFiles 
 removeFiles
-
-#	rm -rf commonappdata
-# sed 's/^"\(.*\)".*/\1/'
 
